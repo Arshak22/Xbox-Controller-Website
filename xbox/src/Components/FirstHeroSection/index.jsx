da95e475b67c96aa648cd9605c88a08f5a3503a8
Arshak22 <arshak_kosakyan@edu.aua.am>
2023-08-02 00:40:04 +0400

    added Images

--- xbox/src/Components/FirstHeroSection/index.jsx
+++ xbox/src/Components/FirstHeroSection/index.jsx
@@ -4,6 +4,8 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Stage } from '@react-three/drei';
 import FirstHeroSectionThree from '../FirstHeroSectionThree';
 
+import FirstFrame from '../../assets/images/Square_4.png';
+
 export default function FirstHeroSection() {
   return (
     <div className='firstHeroSection'>
@@ -11,20 +13,19 @@ export default function FirstHeroSection() {
         <h1 style={{ color: 'white' }}>Something</h1>
       </div>
       <div className='col-1R'>
-        <Canvas
-          style={{ width: 'auto', height: '600px' }}
-        >
-          <ambientLight intensity={0.8} color='#39FF14' />
+        <img src={FirstFrame} alt="firstFrame" className='firstFrame' />
+        <Canvas>
+          <ambientLight intensity={0.8} color='#065e00' />
           <directionalLight
             intensity={0.8}
-            color='#39FF14'
+            color='#065e00'
           />
           <Stage environment='city' intensity={0.05}>
             <FirstHeroSectionThree />
           </Stage>
-          <OrbitControls enableZoom={false} enableDamping={true} />
+          <OrbitControls enableZoom={false} enableDamping={true} rotateSpeed={0.4} />
         </Canvas>
       </div>
-    </div>
+    </div>  
   );
 }
--- xbox/src/Components/FirstHeroSectionThree/index.jsx
+++ xbox/src/Components/FirstHeroSectionThree/index.jsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { useGLTF } from '@react-three/drei';
 // import * as THREE from 'three';
 // import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { useGLTF } from '@react-three/drei';
 
 export default function FirstHeroSectionThree(props) {
   const { nodes, materials } = useGLTF('/xboxOne-transformed.glb');
   const initialRotation = [0.1, 4, 0];
   materials.Material_29.color.set('#39FF14');
+
+  const myMesh1 = useRef({});
+  const myMesh2 = useRef({});
+  const myMesh3 = useRef({});
+  const myMesh4 = useRef({});
+  const myMesh5 = useRef({});
+  const myMesh6 = useRef({});
+  const myMesh7 = useRef({});
+  const myMesh8 = useRef({});
+  const myMesh9 = useRef({});
+
+  useFrame(({ clock }) => {
+    // const a = clock.getElapsedTime();
+    // myMesh1.current.rotation.z = a;
+    // myMesh2.current.rotation.z = a;
+    // myMesh3.current.rotation.z = a;
+    // myMesh4.current.rotation.z = a;
+    // myMesh5.current.rotation.z = a;
+    // myMesh6.current.rotation.z = a;
+    // myMesh7.current.rotation.z = a;
+    // myMesh8.current.rotation.z = a;
+    // myMesh9.current.rotation.z = a;
+  });
+
   return (
     <group {...props} dispose={null} rotation={initialRotation}>
       <mesh
+        ref={myMesh1}
         geometry={nodes['45_Bumpers_Triggers_Material_#40_0'].geometry}
         material={materials.PaletteMaterial001}
         position={[-0.077, 0, -0.076]}
         rotation={[-Math.PI / 2, 0, -Math.PI / 2]}
         scale={0.015}
-      />
+      /> 
       <mesh
+        ref={myMesh2}
         geometry={nodes['46_Dpad_Material_#32_0'].geometry}
         material={materials.Material_32}
         position={[0.001, -0.005, -0.076]}
@@ -25,6 +52,7 @@ export default function FirstHeroSectionThree(props) {
         scale={0}
       />
       <mesh
+        ref={myMesh3}
         geometry={nodes['54_Button_Covers_Material_#66_0'].geometry}
         material={materials.PaletteMaterial002}
         position={[-0.077, 0, -0.076]}
@@ -32,6 +60,7 @@ export default function FirstHeroSectionThree(props) {
         scale={0.015}
       />
       <mesh
+        ref={myMesh4}
         geometry={nodes['59_Port_1_Material_#54_0'].geometry}
         material={materials.Material_54}
         position={[-0.077, 0, -0.076]}
@@ -39,6 +68,7 @@ export default function FirstHeroSectionThree(props) {
         scale={0.015}
       />
       <mesh
+        ref={myMesh5}
         geometry={nodes['60_Port_2_Material_#41_0'].geometry}
         material={materials.Material_41}
         position={[0.001, -0.005, -0.076]}
@@ -46,6 +76,7 @@ export default function FirstHeroSectionThree(props) {
         scale={0}
       />
       <mesh
+        ref={myMesh6}
         geometry={nodes['61_Audio_Port_Material_#37_0'].geometry}
         material={materials.Material_37}
         position={[0.001, -0.005, -0.076]}
@@ -53,6 +84,7 @@ export default function FirstHeroSectionThree(props) {
         scale={0}
       />
       <mesh
+        ref={myMesh7}
         geometry={nodes['64_Front_Case_Material_#29_0'].geometry}
         material={materials.Material_29}
         position={[-0.077, 0, -0.076]}
@@ -60,6 +92,7 @@ export default function FirstHeroSectionThree(props) {
         scale={0.015}
       />
       <mesh
+        ref={myMesh8}
         geometry={nodes['49_Thumstickbot_Material_#53_0'].geometry}
         material={materials.Material_53}
         position={[-0.077, 0, -0.076]}
@@ -67,6 +100,7 @@ export default function FirstHeroSectionThree(props) {
         scale={0.015}
       />
       <mesh
+        ref={myMesh9}
         geometry={nodes['66_Nexus_Center1_Material_#52_0'].geometry}
         material={materials.PaletteMaterial003}
         position={[-0.077, 0, -0.076]}
