04d554251ff75acb691fe41fbc5b705db19738c7
Arshak22 <arshak_kosakyan@edu.aua.am>
2023-08-02 21:41:47 +0400

    Firs Section Done and some changes

--- xbox/src/Components/FirstHeroSection/index.jsx
+++ xbox/src/Components/FirstHeroSection/index.jsx
@@ -4,28 +4,36 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Stage } from '@react-three/drei';
 import FirstHeroSectionThree from '../FirstHeroSectionThree';
 
-import FirstFrame from '../../assets/images/Square_4.png';
+import FirstFrame from '../../assets/images/Square_1.png';
 
 export default function FirstHeroSection() {
   return (
     <div className='firstHeroSection'>
       <div className='col-1L'>
-        <h1 style={{ color: 'white' }}>Something</h1>
+        <h1>Unleash Your Gaming Potential</h1>
+        <a href="#" className='neon-btn'>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Start Gaming
+        </a>
       </div>
       <div className='col-1R'>
-        <img src={FirstFrame} alt="firstFrame" className='firstFrame' />
+        <img src={FirstFrame} alt='firstFrame' className='firstFrame' />
         <Canvas>
-          <ambientLight intensity={0.8} color='#065e00' />
-          <directionalLight
-            intensity={0.8}
-            color='#065e00'
-          />
+          <ambientLight intensity={0.8} color='#10ff00' />
+          <directionalLight intensity={0.8} color='#10ff00' />
           <Stage environment='city' intensity={0.05}>
             <FirstHeroSectionThree />
           </Stage>
-          <OrbitControls enableZoom={false} enableDamping={true} rotateSpeed={0.4} />
+          <OrbitControls
+            enableZoom={false}
+            enableDamping={true}
+            rotateSpeed={0.4}
+          />
         </Canvas>
       </div>
-    </div>  
+    </div>
   );
 }
