b4a86e385af77dfa58b65fb8c70fa0ec331a35ca
Arshak22 <arshak_kosakyan@edu.aua.am>
2023-08-03 23:42:28 +0400

    warning fix

--- xbox/src/Components/ImportantInfo/index.jsx
+++ xbox/src/Components/ImportantInfo/index.jsx
@@ -11,10 +11,10 @@ export default function ImportantInfo() {
             <div className='col-2R'>
                 <h1>Seamless Control<br/>Superior Play</h1>
                 <p>Experience gaming like never before with Xbox Gamepads that redefine precision, elevate your gaming experience, and effortlessly adapt to your playstyle. Our controllers are the ultimate gaming companions that surpass your expectations, allowing you to conquer virtual worlds with ease.</p>
-                <h3 class="underline-hover">Wireless Freedom and Versatility</h3>
-                <h3 class="underline-hover">Quality at its Core</h3>
-                <h3 class="underline-hover">Intuitive and Easy to Use</h3>
-                <h3 class="underline-hover">Ergonomic Excellence</h3>
+                <h3 className="underline-hover">Wireless Freedom and Versatility</h3>
+                <h3 className="underline-hover">Quality at its Core</h3>
+                <h3 className="underline-hover">Intuitive and Easy to Use</h3>
+                <h3 className="underline-hover">Ergonomic Excellence</h3>
             </div>
         </div>
     );
