4e1a7d2ea1415b1c42165ad83c8d6cfc98b9c0ae
Arshak22 <arshak_kosakyan@edu.aua.am>
2023-08-08 01:47:40 +0400

    Started working on slider section

--- xbox/src/App.js
+++ xbox/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import FirstHeroSection from "./Components/FirstHeroSection";
 import ImportantInfo from "./Components/ImportantInfo";
+import HighlightedProducts from "./Components/HighlightedProducts";
+
 
 function App() {
   return (
     <div>
       <FirstHeroSection/>
       <ImportantInfo/>
+      <HighlightedProducts/>
     </div>
   );
 }
---
+++ xbox/src/Components/HighlightedProducts/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './style.css';
+
+export default function HighlightedProducts() {
+    return (
+        <div className="highlightedProducts">
+            
+        </div>
+    );
+}
